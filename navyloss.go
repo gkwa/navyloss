a06527d75ffda909a8a812512de73a9e453b90e7
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-02 14:59:08 -0500

    working

--- navyloss.go
+++ navyloss.go
@@ -1,7 +1,12 @@
 package navyloss
 
 import (
+	"fmt"
 	"log/slog"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
 
 	"github.com/jessevdk/go-flags"
 )
@@ -9,6 +14,7 @@ import (
 var opts struct {
 	LogFormat string `long:"log-format" choice:"text" choice:"json" default:"text" description:"Log format"`
 	Verbose   []bool `short:"v" long:"verbose" description:"Show verbose debug information, each -v bumps log level"`
+	Age       string `short:"a" long:"age" description:"Age parameter in the format 1y, 10M, 10m, 200s, 34d, 1y23d, 2d20s, etc." required:"true"`
 	logLevel  slog.Level
 }
 
@@ -39,10 +45,52 @@ func parseFlags() error {
 }
 
 func run() error {
-	slog.Debug("Debug", "currrent level", opts.logLevel)
-	slog.Info("Info", "currrent level", opts.logLevel)
-	slog.Warn("Warn", "currrent level", opts.logLevel)
-	slog.Error("Error", "currrent level", opts.logLevel)
+	seconds, err := AgeToSeconds(opts.Age)
+	if err != nil {
+		fmt.Printf("Error parsing age parameter: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s = %f seconds\n", opts.Age, seconds)
+
+	t := showDateGivenSecondsAgo(time.Now(), seconds)
+	fmt.Printf("Date %d seconds ago: %s\n", int64(seconds), t.Format(time.RFC3339))
 
 	return nil
+}
+
+func AgeToSeconds(age string) (float64, error) {
+	re := regexp.MustCompile(`(\d+(\.\d+)?)([yMmdsw])`)
+	matches := re.FindAllStringSubmatch(age, -1)
+
+	var totalSeconds float64
+
+	for _, match := range matches {
+		value, err := strconv.ParseFloat(match[1], 64)
+		if err != nil {
+			return 0, err
+		}
+
+		unit := match[3]
+		switch unit {
+		case "y":
+			totalSeconds += value * (60 * time.Minute * 24 * 365).Seconds() // seconds in a year
+		case "M":
+			totalSeconds += value * (60 * time.Minute * 24 * 7 * 30).Seconds() // seconds in a month
+		case "w":
+			totalSeconds += value * (60 * time.Minute * 24 * 7).Seconds() // seconds in a week
+		case "d":
+			totalSeconds += value * (60 * time.Minute * 24).Seconds() // seconds in a day
+		case "m":
+			totalSeconds += value * (1 * time.Minute * 60).Seconds() // seconds in a minute
+		case "s":
+			totalSeconds += value
+		}
+	}
+
+	return totalSeconds, nil
+}
+
+func showDateGivenSecondsAgo(currentTime time.Time, seconds float64) time.Time {
+	return currentTime.Add(-time.Second * time.Duration(seconds)).Truncate(time.Second)
 }
